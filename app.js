5d8fb678e1625643945b8a1ccd2151861ccb2664
eduardomartinez73119@gmail.com <eduardomartinez73119@gmail.com>
2023-03-02 00:48:55 -0600

    Added Mobile Support

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElementLogos = document.querySelectorAll('.made_by_element');
+hiddenElementLogos.forEach((el) => observer.observe(el));
+
+const hiddenElementSkills = document.querySelectorAll('.element_style');
+hiddenElementSkills.forEach((el) => observer.observe(el));
